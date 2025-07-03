c3c9afbd9aac19518b77c5a70e2b8e7e289abc2a
gargisharma09 <sharma931g@gmail.com>
2025-07-03 22:56:56 +0530

    java

--- java/code.java
+++ java/code.java
@@ -4,7 +4,7 @@ public class code {
     * This will print 'Hello World' as the output
     */
 
-   public static void main(String []args) {
+   public static void main(String []args) {}
       System.out.println("Hello World"); // prints Hello World
    }
 }
