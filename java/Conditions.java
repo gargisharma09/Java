f87849b51b25633db4713046c371c51053e25d3a
gargisharma09 <sharma931g@gmail.com>
2025-07-11 16:25:17 +0530

    conditions

--- java/Conditions.java
+++ java/Conditions.java
@@ -19,7 +19,7 @@
    */
 
 // conditionl stats
-import java.util.Scanner;
+/*import java.util.Scanner;
 
 
 public class Conditions {
@@ -36,4 +36,21 @@ public class Conditions {
    
       
    }  
+}*/
+//eveen or odd
+import java.util.*;
+
+public class Conditions {        
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int X = sc.nextInt();
+      if (X % 2 == 0) {
+         System.out.println("even");
+      } else {
+         System.out.println("odd");
+      }
+      sc.close();
+
+   }
 }
+
