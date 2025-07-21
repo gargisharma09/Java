ec9e293eaff7bf6a11dd3c0c2442be438daeb662
gargisharma09 <sharma931g@gmail.com>
2025-07-21 22:57:38 +0530

    java

---
+++ java/string.java
@@ -0,0 +1,13 @@
+public class string {
+
+    public static void main(String[] args) {
+
+        // String declaration and initialization
+        String greeting = "Hello, World!";
+
+        // Print the string
+        System.out.println(greeting);
+    }
+    
+    
+}
--- java/variables.java
+++ java/variables.java
@@ -1,3 +1,11 @@
 public class variables {
-    
+
+    public static void main(String[] args) {
+
+        // Variable declaration and initialization
+
+        int a = 10; // Integer variable
+        System.out.println( a);
+}
+
 }
